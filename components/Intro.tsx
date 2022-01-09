39e7ef2455f2e5c6cf0cfe48083a4b1f36545007
Professor Taki <shakman21@gmail.com>
2022-01-09 18:34:34 -0500

    3

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,10 +2,8 @@ function Intro() {
 
     return (
         <>
-            <div className="bg-intro-bg bg-cover bg-no-repeat max-w-full h-96 mt-4 mx-8 rounded-2xl">
-                <div className="flex flex-col text-4xl p-8">
-                    <h1>3D NFT Marketplace</h1>
-                </div>
+            <div className="bg-intro-bg bg-cover bg-no-repeat max-w-full h-96 mt-4 mx-8 rounded-2xl flex flex-col align-middle">
+                <h1 className="text-4xl p-8">3D NFT Marketplace</h1>
             </div>
         </>
     )
