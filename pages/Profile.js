3cd4ca8e659dea9f38f2884f4746a481e39fe4ee
Professor Taki <shakman21@gmail.com>
2022-01-22 17:38:50 -0500

    8

--- pages/Profile.js
+++ pages/Profile.js
@@ -29,28 +29,30 @@ const styles = {
   },
 };
 
-var userNFTs = [] 
-
 function Loader() {
   const { active, progress, errors, item, loaded, total } = useProgress();
   return <Html center>{progress} % loaded</Html>;
 }
 
 export default function Profile() {
+  const [userNFTs, setUserNFTs] = useState([])
   const { enableWeb3, Moralis, isAuthenticated, user} = useMoralis();
   /*const account = useMoralisWeb3Api();
     const { web3, enableWeb3, Moralis } = useMoralis();
     const { error, isUploading, moralisFile, saveFile } = useMoralisFile();
     const { getNFTBalances, data, isLoading, isFetching } = useNFTBalances();*/
+
+    async function queryNFTs() {
+      const query = new Moralis.Query('AvaxNFTOwners')
+      query.equalTo('owner_of', user.get("ethAddress")) // fileName depends on NFT 
+      console.log("who asked: ", await query.find())
+    }
     
   async function getUserNFTs () {
     if(isAuthenticated) {
       const nfts = await Moralis.Web3API.account.getNFTs({chain: 'avalanche testnet', address: user.get("ethAddress") })
-
-      nfts.result.forEach(element => {
-        userNFTs.push(element)
-      });
-      
+      setUserNFTs(nfts.result)
+      console.log(nfts.result)
     }
   }
 
@@ -58,17 +60,17 @@ export default function Profile() {
 
   }
 
-  console.log("PLEASEPLEASEPLEASE")
-  console.log(userNFTs)
-
-  getUserNFTs()
+  useEffect(() => {
+    getUserNFTs()
+    queryNFTs()
+  }, [])
 
       return (
         <div className='flex w-full'>
-          {userNFTs.forEach((nft) => (
-            <div key={5}>
+          {userNFTs.map((nft, index) => (
+            <div key={index}>
               <div className='h-screen w-1/2'>
-                <h1>NFTs: {nft.name}</h1>
+                <h1>NFTs : {nft.name}</h1>
                 <Canvas>
                   <Suspense fallback={<Loader />}> 
                   <ambientLight intensity={0.2} />
